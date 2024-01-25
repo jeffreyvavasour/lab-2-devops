0fae6b84ac0a621f900bc77ff55103adea45fd90
Jeffrey Vavasour <jeffreyvavasour@gmail.com>
2024-01-25 21:03:00 +0000

    updated script to delete folders

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-touch fol_1/
-touch fol_2/
+mkdir fol_1/
+mkdir fol_2/
 touch fol_1/1_1.txt
 touch fol_1/1_2.txt
 touch fol_1/1_3.txt
 touch fol_2/2_1.txt
 touch fol_2/2_2.txt
 touch fol_2/2_3.txt
+rm -r fol_1/
+rm -r fol_2/
