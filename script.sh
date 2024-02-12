fbbf0da7eda18dd7769579d16552bd1dbc5e1195
Jeffrey Vavasour <jeffreyvavasour@gmail.com>
2024-02-12 18:34:28 +0000

    updated script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-mkdir fol_1/
-mkdir fol_2/
-touch fol_1/1_1.txt
-touch fol_1/1_2.txt
-touch fol_1/1_3.txt
-touch fol_2/2_1.txt
-touch fol_2/2_2.txt
-touch fol_2/2_3.txt
-rm -r fol_1/
-rm -r fol_2/
+rm -r fol_1/ fol_2/
+mkdir fol_1/ fol_2/
+touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+chmod 700 fol_1/*_1.txt fol_1/*_3.txt
+chmod 777 fol_1/*_2.txt
+chmod 700 fol_2/*_1.txt fol_2/*_3.txt
+chmod 777 fol_2/*_2.txt
